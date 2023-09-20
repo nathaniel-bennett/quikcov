56efcbf5abfb111fe5eb7aeafa76d2b438967b64
Nathaniel Bennett <me@nathanielbennett.com>
2023-09-20 19:48:03 -0400

    Add variadic function def

--- preload/src/lib.rs
+++ preload/src/lib.rs
@@ -13,14 +13,15 @@ pub const QUIKCOV_PIPE_ENV: &str = "QUIKCOV_LDPRELOAD_PIPE_FD";
 
 // FIXME: what if the variadic argument `mode` isn't used? Could lead to UB...
 
+/*
 hook_macros::hook! {
     unsafe fn openat(
         dirfd: libc::c_int,
         pathname: *const libc::c_char,
         flags: libc::c_int,
-        ...
+        args: ...
     ) -> libc::c_int => quikcov_openat {
-        let ret = hook_macros::real!(openat)(dirfd, pathname, flags, ...);
+        let ret = hook_macros::real!(openat)(dirfd, pathname, flags, args);
 
         let path_cstr = unsafe { CStr::from_ptr(pathname) };
         let len = path_cstr.to_bytes().len();
@@ -37,6 +38,68 @@ hook_macros::hook! {
         ret
     }
 }
+*/
+
+// We can't use hook_macros::hook! here as it doesn't support variadics
+
+pub struct OpenatStruct {__private_field: ()}
+static OPENAT_CONST: OpenatStruct = OpenatStruct {__private_field: ()};
+
+impl OpenatStruct {
+    fn get(&self) -> unsafe extern fn (dirfd: libc::c_int, pathname: *const libc::c_char, flags: libc::c_int, args: ...) -> libc::c_int {
+        use ::std::sync::Once;
+
+        static mut REAL: *const u8 = 0 as *const u8;
+        static mut ONCE: Once = Once::new();
+
+        unsafe {
+            ONCE.call_once(|| {
+                REAL = hook_macros::ld_preload::dlsym_next(concat!(stringify!($real_fn), "\0"));
+            });
+            ::std::mem::transmute(REAL)
+        }
+    }   
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn openat(dirfd: libc::c_int, pathname: *const libc::c_char, flags: libc::c_int, args: ...) -> libc::c_int {
+    let orig_hook = std::panic::take_hook();
+
+    // in case openat_quikcov panics, this will immediately abort rather than unwinding and causing UB.
+    std::panic::set_hook(Box::new(move |panic_info| {
+        std::process::abort()
+    }));
+    let ret = openat_quikcov(dirfd, pathname, flags, args);
+
+    // Reset the panic handler hook
+    std::panic::set_hook(orig_hook);
+    
+    ret
+}
+
+
+
+pub unsafe extern fn openat_quikcov(dirfd: libc::c_int, pathname: *const libc::c_char, flags: libc::c_int, args: ...) -> libc::c_int {
+    let ret = OPENAT_CONST.get()(dirfd, pathname, flags, args);
+
+    let path_cstr = unsafe { CStr::from_ptr(pathname) };
+    let len = path_cstr.to_bytes().len();
+
+    let is_gcda = path_cstr.to_bytes().get(len.saturating_sub(5)..).map(|suffix| suffix == b".gcda".as_slice()).unwrap_or(false);
+
+    if is_gcda && ret >= 0 {
+        state::gcda_files().lock().unwrap().insert(ret, FileInfo {
+            path: path_cstr.to_str().unwrap().to_string(),
+            data: Vec::new(),
+        });
+    }
+
+    ret
+}
+
+
+
+
 
 hook_macros::hook! {
     unsafe fn write(
