f81c3008ea5e516582270dcde9d9b2e85fdc4f40
Nathaniel Bennett <me@nathanielbennett.com>
2023-09-21 15:20:30 -0400

    Remove old code

--- quikcov/src/main.rs
+++ quikcov/src/main.rs
@@ -17,9 +17,11 @@ const QUIKCOV_PIPE_ENV: &str = "QUIKCOV_LDPRELOAD_PIPE_FD";
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+/*
     /// The directory containing the source code of the program
     #[arg(long, value_name = "PATH")]
     source_path: String,
+*/
     /// The directory containing .gcno and .gcda files for the program
     #[arg(long, value_name = "PATH")]
     cov_path: String,
@@ -219,35 +221,3 @@ struct CoverageFunction {
     covered_blocks: usize,
     total_blocks: usize,
 }
-
-
-
-fn test() {
-    env_logger::init();
-    let mut args = std::env::args();
-
-    if args.len() != 4 {
-        println!("Usage: quikcov <gcno> <gcda> <outfile>");
-        return
-    }
-    args.next();
-    let gcno_file = args.next().unwrap().to_string();
-    let gcda_file = args.next().unwrap().to_string();
-    let outfile = args.next().unwrap().to_string();
-    
-    let gcno_bytes = std::fs::read(gcno_file).unwrap();
-    let gcda_bytes = std::fs::read(gcda_file).unwrap();
-
-    let gcno = Gcno::from_slice(gcno_bytes.as_slice()).unwrap();
-    let mut cov_builder = FileCovBuilder::new(gcno);
-    cov_builder.add_gcda(gcda_bytes.as_slice()).unwrap();
-    let cov = cov_builder.build().unwrap();
-
-//    let bytes: Vec<u8> = postcard::to_stdvec(&cov).unwrap();
-    let bytes = serde_json::to_vec(&cov).unwrap();
-    std::fs::write(outfile, bytes).unwrap();
-}
-
-
-
-
